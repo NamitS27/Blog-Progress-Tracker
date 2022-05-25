3b241e51128222b682d31315ef3d94e6e3c0e341
NamitS27 <namitshah2711@gmail.com>
2022-05-25 17:06:43 +0530

    node-express server setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog Progress Tracker
+
+Blog progress trackers tracks the progress of a blog for a particular user along with tracking the progress of different content types.
---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+/**
+ * Configuring the environmental varaibles
+ */
+var dotenv = require('dotenv');
+dotenv.config();
+
+/**
+ * Forming the mongo URI for connecting to the database
+ */
+var mongoose = require('mongoose');
+var password = encodeURIComponent(process.env.PASSWORD);
+var username = process.env.DBUSER;
+const mongo_uri = `mongodb+srv://${username}:${password}@lyearn.ipvec.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(mongo_uri);
+
+/**
+ * Connecting to the mongo db database
+ */
+mongoose.connect(
+	mongo_uri,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	},
+	(err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Connected to Lyearn Database');
+		}
+	}
+);
+
+var app = express();
+
+/**
+ * Importing the routes for different end-points
+ * < progress >
+ */
+let progress = require('./routes/blog');
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// for cookie parser
+app.use(cookieParser());
+
+// logging the request being sent to the API
+app.use(logger('dev'));
+
+// CORS for cross-origin application
+app.use(cors());
+app.options('*', cors());
+
+/**
+ * Specifying the routes for the different functionality
+ * default route: /ohodr/api/
+ */
+app.use('/lyearn/api/v1/progress', progress);
+
+module.exports = app;
